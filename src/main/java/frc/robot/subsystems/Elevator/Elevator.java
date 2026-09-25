2676f06945c6f11d3bd187b855f8a249c722333b
CJPoppaDoppa <164113849+laurendt@users.noreply.github.com>
2026-09-25 00:09:07 -0400

    changed a few things just have to set controls and etc
    i am super tired hope it works......

--- src/main/java/frc/robot/subsystems/Elevator/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator/Elevator.java
@@ -1,12 +1,3 @@
 package frc.robot.subsystems.elevator;
 
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.trajectory.TrapezoidProfile;
-import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-
-public class Elevator {
-    
-    
-}
+public class Elevator {}
--- src/main/java/frc/robot/subsystems/Elevator/ElevatorConstants.java
+++ src/main/java/frc/robot/subsystems/Elevator/ElevatorConstants.java
@@ -1,18 +1,18 @@
 package frc.robot.subsystems.elevator;
 
 public class ElevatorConstants {
-    // we don't need setpoints right now
-    public static final int CAN_ID_LEFT = 0;
-    public static final int CAN_ID_RIGHT = 0;
+  // we don't need setpoints right now
+  public static final int CAN_ID_LEFT = 0;
+  public static final int CAN_ID_RIGHT = 0;
 
-    public static final double GOAL_TOLERANCE = 0.02;
-    public static final double STOW = 0.005;
+  public static final double GOAL_TOLERANCE = 0.02;
+  public static final double STOW = 0.005;
 
-    public static final double kD = 0;
-    public static final double kI = 0;
-    public static final double kP = 0; 
-    public static final double kG = 0;
-    public static final double kS = 0;
-    public static final double kV = 0;
-    public static final double kA = 0;
+  public static final double kD = 0;
+  public static final double kI = 0;
+  public static final double kP = 0;
+  public static final double kG = 0;
+  public static final double kS = 0;
+  public static final double kV = 0;
+  public static final double kA = 0;
 }
